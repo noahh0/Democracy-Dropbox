27741fcfb37a56971fcee4b8e68110d48535b264
krishna-saxena <ksaxena3@uw.edu>
2024-11-19 20:39:09 -0800

    add unweighted models as well

--- src/analysis/weighted_ols.py
+++ src/analysis/weighted_ols.py
@@ -44,6 +44,14 @@ merged_df['prop_voted'] = merged_df['num_voted']/merged_df['total_voting_age_pop
 
 
 # SECTION 3: Data Analysis
+def get_ols(X, y):
+  beta_hat, code = bicgstab(X.T @ X, X.T @ y)
+  assert code == 0
+
+  y_hat = X @ beta_hat
+  r_sq = 1 - np.var(y - y_hat, ddof=0) / np.var(y, ddof=0)
+  return beta_hat, y_hat, r_sq
+
 def get_weighted_ols(X, y, W):
   beta_hat, code = bicgstab(X.T @ W @ X, X.T @ W @ y)
   assert code == 0
@@ -52,7 +60,7 @@ def get_weighted_ols(X, y, W):
   r_sq = 1 - np.var(W @ (y - y_hat), ddof=0) / np.var(W @ y, ddof=0)
   return beta_hat, y_hat, r_sq
 
-def plot_weighted_ols(X, y, beta_hat, r_sq):
+def plot_ols(X, y, beta_hat, r_sq, model_name = ''):
   plt.scatter(X[:, 0], y)
   max_dist = np.max(X[:, 0])
   plt.plot([0, max_dist], [beta_hat[1], beta_hat[0] * max_dist + beta_hat[1]],
@@ -60,36 +68,44 @@ def plot_weighted_ols(X, y, beta_hat, r_sq):
            label='$\hat{p} = $' + f'{beta_hat[0]:.4e} d + {beta_hat[1]:.4e}\n($r^2$ = {r_sq:0.4f})')
   plt.xlabel('Distance to Nearest Ballot Dropbox')
   plt.ylabel('Proportion of Voters who Turned Out')
-  plt.suptitle('Voter Turnout vs Mean Distance to Dropbox')
+  plt.suptitle(f'Voter Turnout vs Mean Distance to Dropbox {model_name}')
   plt.title('in WA Voting Blocks for the 2020 General Election')
   plt.legend()
 
-# data analysis 1- no processing
+# analysis 0- unweighted model, no processing
 N = merged_df.shape[0]
 
 # explanatory variables are the distance to a drop box and bias
 X = np.stack((merged_df['dist_to_nearest_ballot_box_km'], np.ones((N, ))), axis=-1)
-# weight districts by their count of eligible voters
-W = spdiags(merged_df['total_voting_age_pop'], [0], N, N)
 # the response variable is the proportion of voters who voted
 y = merged_df['prop_voted']
 
-beta_hat, y_hat, r_sq = get_weighted_ols(X, y, W)
-plot_weighted_ols(X, y, beta_hat, r_sq)
+beta_hat, y_hat, r_sq = get_ols(X, y)
+plot_ols(X, y, beta_hat, r_sq, '(Unweighted Model)')
 plt.tight_layout()
 plt.show()
 
-# data analysis 2- cap proportions to 1
-N = merged_df.shape[0]
-
-# explanatory variables are the distance to a drop box and bias
-X = np.stack((merged_df['dist_to_nearest_ballot_box_km'], np.ones((N, ))), axis=-1)
+# analysis 1- weighted model, no processing
 # weight districts by their count of eligible voters
 W = spdiags(merged_df['total_voting_age_pop'], [0], N, N)
-# the response variable is the proportion of voters who voted
-y = merged_df['prop_voted'].where(merged_df['prop_voted'] < 1., 1.)
 
 beta_hat, y_hat, r_sq = get_weighted_ols(X, y, W)
-plot_weighted_ols(X, y, beta_hat, r_sq)
+plot_ols(X, y, beta_hat, r_sq, '(Weighted Model)')
+plt.tight_layout()
+plt.show()
+
+# analysis 2- unweighted model, cap proportions to 1
+y_cap = merged_df['prop_voted'].where(merged_df['prop_voted'] < 1., 1.)
+print('Max y cap', max(y_cap))
+
+beta_hat, y_hat, r_sq = get_ols(X, y_cap)
+plot_ols(X, y_cap, beta_hat, r_sq, '(Unweighted Model)')
+plt.tight_layout()
+plt.show()
+
+# analysis 3- cap proportions to 1
+# the response variable is the proportion of voters who voted, capped to 1
+beta_hat, y_hat, r_sq = get_weighted_ols(X, y_cap, W)
+plot_ols(X, y_cap, beta_hat, r_sq, '(Weighted Model)')
 plt.tight_layout()
 plt.show()
